e387ca1ac133c78e29a68fa296a19f9649d57ac5
NMSaiTharun <nmsaitarun@gmail.com>
2026-06-17 09:17:00 +0100

    login page-ui fixes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,33 @@
+import { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
       <Header />
-      <div className="absolute">
+      <div className="absolute w-full">
         <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/8027eb3f-343a-499d-9892-e683c12e3cb1/web/IE-en-20260608-TRIFECTA-perspective_8be89c0e-d22e-485b-ac7b-0e15972cbea6_large.jpg"
-          alt="logo"
-        />
-        <img
+          className="w-full"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/8027eb3f-343a-499d-9892-e683c12e3cb1/web/IE-en-20260608-TRIFECTA-perspective_8be89c0e-d22e-485b-ac7b-0e15972cbea6_large.jpg"
           alt="logo"
         />
       </div>
-      <form className="w-4/12 absolute p-12 bg-black my-90 mx-auto right-0 left-0 text-white opacity-80">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+      <form className="w-2/12 absolute p-12 bg-black my-100 mx-300 text-white opacity-80">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+
         <input
           type="text"
           placeholder="Email Address"
@@ -26,10 +38,14 @@ const Login = () => {
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
         />
-        <button className="p-4 m-4 cursor-pointer bg-red-700 w-full rounded-lg">
-          Sign In
+        <button className="p-4 my-4 cursor-pointer bg-red-700 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
-        <p>New to Netflix? Sign Up Now</p>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already Registered? Sign In Now"}
+        </p>
       </form>
     </div>
   );
