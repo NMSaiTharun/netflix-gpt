280424bae15016fed3636d8367774224c54a9741
NMSaiTharun <nmsaitarun@gmail.com>
2026-06-17 19:31:17 +0100

    responsive UI

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,46 +7,47 @@ const Login = () => {
     setIsSignInForm(!isSignInForm);
   };
   return (
-    <div>
+    <div className="relative h-screen">
       <Header />
-      <div className="absolute w-full">
+      <div className="absolute w-full h-full">
         <img
-          className="w-full"
+          className="w-full h-full object-cover"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/8027eb3f-343a-499d-9892-e683c12e3cb1/web/IE-en-20260608-TRIFECTA-perspective_8be89c0e-d22e-485b-ac7b-0e15972cbea6_large.jpg"
           alt="logo"
         />
       </div>
-      <form className="w-2/12 absolute p-12 bg-black my-100 mx-300 text-white opacity-80">
-        <h1 className="font-bold text-3xl py-4">
-          {isSignInForm ? "Sign In" : "Sign Up"}
-        </h1>
-        {!isSignInForm && (
+      <div className="absolute w-full h-full flex justify-center items-center">
+        <form className="w-full sm:w-8/12 md:w-5/12 lg:w-3/12 p-12 bg-black text-white opacity-90 rounded-lg">
+          <h1 className="font-bold text-3xl py-4">
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </h1>
+          {!isSignInForm && (
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="p-4 my-4 w-full bg-gray-700"
+            />
+          )}
           <input
             type="text"
-            placeholder="Full Name"
+            placeholder="Email Address"
             className="p-4 my-4 w-full bg-gray-700"
           />
-        )}
-
-        <input
-          type="text"
-          placeholder="Email Address"
-          className="p-4 my-4 w-full bg-gray-700"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="p-4 my-4 w-full bg-gray-700"
-        />
-        <button className="p-4 my-4 cursor-pointer bg-red-700 w-full rounded-lg">
-          {isSignInForm ? "Sign In" : "Sign Up"}
-        </button>
-        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
-          {isSignInForm
-            ? "New to Netflix? Sign Up Now"
-            : "Already Registered? Sign In Now"}
-        </p>
-      </form>
+          <input
+            type="password"
+            placeholder="Password"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+          <button className="p-4 my-4 cursor-pointer bg-red-700 w-full rounded-lg">
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </button>
+          <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+            {isSignInForm
+              ? "New to Netflix? Sign Up Now"
+              : "Already Registered? Sign In Now"}
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
